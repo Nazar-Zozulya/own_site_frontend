4056204357536bd1c0d2d5e7c5e4b16dbc8b1065
Nazar Zozulya <likeemangames@gmail.com>
2025-09-13 22:26:45 +0300

    add docker and nginx

--- src/modules/contact-me-part/ui/contact-me-part/part.tsx
+++ src/modules/contact-me-part/ui/contact-me-part/part.tsx
@@ -1,16 +1,41 @@
+import { useForm } from "react-hook-form"
+import { API_URL } from "../../../../shared/constants"
 import styles from "./part.module.css"
+import { ContactMePartForm } from "./part.types"
 
 export function ContactMePart() {
+	const { handleSubmit, register } = useForm<ContactMePartForm>()
+
+	async function sendMessage(data: ContactMePartForm) {
+
+		const response = await fetch(`${API_URL}contact-me`, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(
+				data,
+			),
+		})
+	}
+
 	return (
 		<div className={styles.container} id="contactMe">
 			<p className={styles.title}>SAY HELLO</p>
-			<p className={styles.content}>
+			<form
+				className={styles.content}
+				onSubmit={handleSubmit(sendMessage)}
+			>
 				<div className={styles.inputAndLabel}>
 					<p className={styles.label}>Name</p>
 					<input
 						type="text"
 						placeholder="Your Name"
 						className={styles.input}
+						{...register("name", {
+							required: {
+								value: true,
+								message: "Write your name",
+							},
+						})}
 					/>
 				</div>
 				<div className={styles.inputAndLabel}>
@@ -19,19 +44,31 @@ export function ContactMePart() {
 						type="text"
 						placeholder="john@doe.com"
 						className={styles.input}
+						{...register("email", {
+							required: {
+								value: true,
+								message: "Write your email",
+							},
+						})}
 					/>
 				</div>
 				<div className={styles.inputAndLabel}>
 					<p className={styles.label}>Message</p>
 					<textarea
-                        rows={4}
+						rows={4}
 						placeholder="Your Message"
 						className={`${styles.input} ${styles.textarea}`}
+						{...register("message", {
+							required: {
+								value: true,
+								message: "Write your message",
+							},
+						})}
 					/>
 				</div>
 
 				<div className={styles.sendButtonHelper}>
-					<button className={styles.sendButton}>
+					<button className={styles.sendButton} type="submit">
 						<svg
 							width="22"
 							height="22"
@@ -47,10 +84,10 @@ export function ContactMePart() {
 								stroke-linejoin="round"
 							/>
 						</svg>
-                        <p>Send</p>
+						<p>Send</p>
 					</button>
 				</div>
-			</p>
+			</form>
 		</div>
 	)
 }
